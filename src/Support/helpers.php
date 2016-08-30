afff8621833540bdb06bae13aa024bc55a7c00f8
Johnathon Koster <john@stillat.com>
2016-08-30 14:13:13 -0500

    Removed unusued helpers

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -10,13 +10,3 @@ function config($key)
 {
 	return Collector\Support\Config::getInstance()->get($key);
 }
-
-function psr4($namespace, $path)
-{
-	Collector\Application::getInstance()->getLoader()->setPsr4($namespace, $path);
-}
-
-function process($process)
-{
-	return new Symfony\Component\Process\Process($process);
-}
