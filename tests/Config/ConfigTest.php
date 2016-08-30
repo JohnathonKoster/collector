e4eb5a9c2fe7d5782196f2b6d256f132ce252144
John Koster <john@stillat.com>
2016-08-30 16:56:45 -0500

    Config system tests

---
+++ tests/Config/ConfigTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use Collector\Support\Config;
+
+class ConfigTest extends PHPUnit_Framework_TestCase
+{
+
+	const CONFIG_DIR = __DIR__.'/../files/sample_config';
+
+	public function testThatConfigCantBeInstantiatedDirectly()
+	{
+		$reflection  = new ReflectionClass(Config::class);
+		$constructor = $reflection->getConstructor();
+		$this->assertFalse($constructor->isPublic());
+	}
+
+	public function testThatConfigReturnsSameInstance()
+	{
+		$instanceOne = Config::getInstance(self::CONFIG_DIR);
+		$instanceTwo = Config::getInstance();
+
+		$this->assertSame($instanceOne, $instanceTwo);
+	}
+
+	public function testThatConfigReturnsCorrectValue()
+	{
+		$config = Config::getInstance(self::CONFIG_DIR);
+		$this->assertEquals($config->get('test.test'), 'value');
+		$this->assertEquals($config->get('example.another'), 'key');
+	}
+
+}
