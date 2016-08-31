5d37dd558927de565820eee9d5686ea8a09d7fc4
Johnathon Koster <john@stillat.com>
2016-08-30 20:57:13 -0500

    Fixes helper redclaration issues

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -1,12 +1,14 @@
 <?php
 
-/**
- * Gets the value for the provided configuration key.
- *
- * @param  string
- * @return mixed
- */
-function config($key)
-{
-	return Collector\Support\Config::getInstance()->get($key);
+if (! function_exists('config')) {
+    /**
+     * Gets the value for the provided configuration key.
+     *
+     * @param  string
+     * @return mixed
+     */
+    function config($key)
+    {
+        return Collector\Support\Config::getInstance()->get($key);
+    }
 }
