fe68197d77a95adc0691da6842f97dcdf966fc8b
Johnathon Koster <john@stillat.com>
2016-08-31 08:47:24 -0500

    Fixing some braces

--- src/Utils/File.php
+++ src/Utils/File.php
@@ -112,59 +112,48 @@ class File
 	}
 
 	/**
-	 * Recursively delete a directory and all of it's contents - e.g.the equivalent of `rm -r` on the command-line.
-	 * Consistent with `rmdir()` and `unlink()`, an E_WARNING level error will be generated on failure.
+	 * Recursively deletes a directory and all it's contents.
 	 * 
 	 * # http://stackoverflow.com/a/3352564/283851
 	 * # https://gist.github.com/XzaR90/48c6b615be12fa765898
 	 * # Forked from https://gist.github.com/mindplay-dk/a4aad91f5a4f1283a5e2
 	 *
-	 * @param string $source absolute path to directory or file to delete.
-	 * @param bool   $removeOnlyChildren set to true will only remove content inside directory.
+	 * @param string $source
+	 * @param bool   $removeOnlyChildren
 	 *
-	 * @return bool true on success; false on failure
+	 * @return bool
 	 */
 	public function deleteDirectory($source, $removeOnlyChildren = false)
 	{
-	    if(empty($source) || file_exists($source) === false)
-	    {
+	    if (empty($source) || file_exists($source) === false) {
 	        return false;
 	    }
 
-	    if(is_file($source) || is_link($source))
-	    {
+	    if (is_file($source) || is_link($source)) {
 	        return unlink($source);
 	    }
 
-	    $files = new RecursiveIteratorIterator
-	    (
+	    $files = new RecursiveIteratorIterator (
 	        new RecursiveDirectoryIterator($source, RecursiveDirectoryIterator::SKIP_DOTS),
 	        RecursiveIteratorIterator::CHILD_FIRST
 	    );
 
 	    //$fileinfo as SplFileInfo
-	    foreach($files as $fileinfo)
-	    {
-	        if($fileinfo->isDir())
-	        {
-	            if($this->deleteDirectory($fileinfo->getRealPath()) === false)
-	            {
+	    foreach ($files as $fileinfo) {
+	        if ($fileinfo->isDir()) {
+	            if ($this->deleteDirectory($fileinfo->getRealPath()) === false) {
 	                return false;
 	            }
-	        }
-	        else
-	        {
+	        } else {
 	        	// Permissions hack, we're just deleting all this stuff anyway right now.
 	        	chmod($fileinfo->getRealPath(), 0777);
-	            if(unlink($fileinfo->getRealPath()) === false)
-	            {
+	            if (unlink($fileinfo->getRealPath()) === false) {
 	                return false;
 	            }
 	        }
 	    }
 
-	    if($removeOnlyChildren === false)
-	    {
+	    if ($removeOnlyChildren === false) {
 	        return rmdir($source);
 	    }
 
