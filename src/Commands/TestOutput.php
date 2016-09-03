c81d935813cf88cfb322a1ca479baaeff4368ad6
John Koster <john@stillat.com>
2016-09-02 19:19:49 -0500

    Test status report table

--- src/Commands/TestOutput.php
+++ src/Commands/TestOutput.php
@@ -4,7 +4,9 @@ namespace Collector\Commands;
 
 use Collector\Utils\Tests\Runner;
 use Collector\Utils\VersionHistoryManager;
+use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -36,10 +38,29 @@ class TestOutput extends Command
 				$output->writeln("<{$type}>{$message}</{$type}>");
 			});
 		}
+		$history = $this->history->getSplitHistory();
+		$data = [];
+		$progressBar = new ProgressBar($output, count($history));
+		$progressBar->setFormat("%message%\n%current%/%max% [%bar%] %percent:3s%% %elapsed:6s%/%estimated:-6s% %memory:6s%");
+		$progressBar->setMessage('Building your report...');
+		foreach ($history as $splitVersion) {
+			$progressBar->setMessage('Running tests on '.$splitVersion.'...');
+			$code = $this->runner->runTestsOn($splitVersion);
+			$message = 'Passed';
 
-		foreach ($this->history->getSplitHistory() as $splitVersion) {
-			$this->runner->runTestsOn($splitVersion);
+			if ($code !== 0) {
+				$message = 'Failed';
+			}
+
+			$data[] = [$splitVersion, $message];
+			$progressBar->setMessage('Running tests on '.$splitVersion.'... '.$message);
+			$progressBar->advance();
 		}
+		$progressBar->finish();
+
+		$table = new Table($output);
+		$table->setHeaders(['Version', 'Passed'])
+		->setRows($data)->render();
 	}
 
 }
